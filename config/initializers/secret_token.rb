481f70bc7814db8ebaec0817dd5cdfb8c26398dd
Paul Richardson <prichardson@universalbiosensors.com>
2014-05-01 14:43:21 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '398366ae755f985d108d12657f2c34c402ed91beea046f0c726873daed29cc6788f9a3fdad0b2bf847cced460e0d9ecb5971e9a505b497947faa602770eea25a'
